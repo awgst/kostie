ef66a1da209ea010473481d91f7297447b211903
awgst <trisaktiawang@gmail.com>
2022-04-21 22:18:12 +0700

    Fix passport user_id use uuid

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/owner/register', [AuthController::class, 'registerOwner']);
 });
