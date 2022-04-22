13a9c476c2969eb8b076033f3fa855071e7a41bf
awgst <trisaktiawang@gmail.com>
2022-04-22 08:35:35 +0700

    [kost] Relation between kost and owner model

--- Modules/Kost/Entities/Kost.php
+++ Modules/Kost/Entities/Kost.php
@@ -2,13 +2,23 @@
 
 namespace Modules\Kost\Entities;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Modules\User\Entities\Owner;
 
 class Kost extends Model
 {
+    use Uuid;
+    
     protected $guarded = ['id'];
 
     protected $connection = 'mysql';
 
     protected $table = 'kosts';
+
+    // Owner Relation
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class, 'owner_id');
+    }
 }
--- Modules/User/Entities/Owner.php
+++ Modules/User/Entities/Owner.php
@@ -2,11 +2,18 @@
 
 namespace Modules\User\Entities;
 
+use Modules\Kost\Entities\Kost;
 use Modules\User\Constants\UserType;
 use Modules\User\Scopes\OwnerScope;
 
 class Owner extends User
 {
+    // Kost relation
+    public function kosts()
+    {
+        return $this->hasMany(Kost::class, 'owner_id', 'id');
+    }
+
     public static function boot()
     {
         static::addGlobalScope(new OwnerScope);
