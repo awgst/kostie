91dc8aac5d1bc2287a9365297fc7cf34aa07532e
awgst <trisaktiawang@gmail.com>
2022-04-22 11:27:39 +0700

    Add primary key to uuid

--- Modules/Kost/Database/Migrations/2022_04_22_070237_create_kosts_table.php.php
+++ Modules/Kost/Database/Migrations/2022_04_22_070237_create_kosts_table.php.php
@@ -14,7 +14,7 @@ class CreateKostsTable extends Migration
     public function up()
     {
         Schema::create('kosts', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->foreignUuid('owner_id');
             $table->string('name');
             $table->string('description');
